7779db4f7e9eeb3054641a6766817de5fe295d41
koregeo <koregm@gmail.com>
2021-03-13 16:43:38 +0400

    Creating Dropdown Menu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
+import React, { useState, useEffect } from 'react'
 import Footer from './Components/Footer';
 import Navbar from './Components/Navbar';
 import Home from './Pages';
-import {Switch, Route} from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import About from './Pages/about';
 import Menu from './Pages/menu';
+import Dropdown from './Components/Dropdown';
 
 function App() {
+  /* Dropdown Menu feature-above this line */
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
+
+
+  useEffect(() => { // <-- hide dropdown menu when screen > 768 & dropdown menu is open
+    const hideMenu = () => {
+      if (window.innerWidth > 768 && isOpen) {
+        setIsOpen(false)
+      }
+    }
+    window.addEventListener('resize', hideMenu)
+    return () => {
+      window.removeEventListener('resize', hideMenu)
+    }
+  })
+  /* Dropdown Menu feature-above this line */
+
+
   return (
     <>
-      <Navbar />
+      <Navbar toggle={toggle} />
+      <Dropdown isOpen={isOpen} toggle={toggle} />
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/menu" exact component={Menu} />
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({ toggle }) => {
     return (
         <nav className="flex justify-between items-center h-16 bg-white text-black relative shadow-sm font-modo"
         role="navigation"
@@ -9,7 +9,7 @@ const Navbar = () => {
             <Link to='/' className="pl-8">
                 EGG
             </Link>
-            <div className="px-4 cursor-pointer md:hidden">
+            <div className="px-4 cursor-pointer md:hidden" onClick={toggle}>
                 <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
             </div>
             <div className="pr-7 md:block hidden">
