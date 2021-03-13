a1d53a29d38652039d543e9152a1d766fdcf73c3
koregeo <koregm@gmail.com>
2021-03-13 14:23:48 +0400

    add nav-bar's Pages & Routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import Content from './Components/Content';
 import Footer from './Components/Footer';
-import Hero from './Components/Hero';
 import Navbar from './Components/Navbar';
+import Home from './Pages';
+import {Switch, Route} from 'react-router-dom'
+import About from './Pages/about';
+import Menu from './Pages/menu';
 
 function App() {
   return (
     <>
       <Navbar />
-      <Hero />
-      <Content />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/menu" exact component={Menu} />
+        <Route path="/about" exact component={About} />
+      </Switch>
       <Footer />
     </>
   );
